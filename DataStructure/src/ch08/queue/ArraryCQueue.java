8ee399082531b6f99809024c699c25c21a81f9c9
gopae02 <gopae02@gmail.com>
2016-04-24 00:14:55 +0900

    16/04/24 Circular Queue

--- DataStructure/src/ch08/queue/ArraryCQueue.java
+++ DataStructure/src/ch08/queue/ArraryCQueue.java
@@ -1,6 +1,6 @@
 package ch08.queue;
 
-public class ArraryQueue implements Queue {
+public class ArraryCQueue implements Queue {
 	
 	private int front;	// 저장된 원소 중에서 첫 번째 원소지정하여 삭제 연산만 수행
 	private int rear;	// 다른쪽 끝은 삽입 연산만 수행하도록 제한
@@ -8,10 +8,10 @@ public class ArraryQueue implements Queue {
 	private char itemArray[];
 	
 	// 초기화 
-	// 새로 생성하여 저장된 원소가 없는 공백큐 이므로 front/rear -1로 초기화 
-	public ArraryQueue(int queueSize){
-		front = -1;	
-		rear = -1;
+	// 초기 공백상태의 원형큐 이므로 front/rear 0로 초기화 
+	public ArraryCQueue(int queueSize){
+		front = 0;	
+		rear = 0;
 		this.queueSize = queueSize;
 		itemArray = new char[this.queueSize];
 	}
@@ -31,9 +31,10 @@ public class ArraryQueue implements Queue {
 	public void enQueue(char item) {
 		// TODO Auto-generated method stub
 		if(isFull())
-			System.out.println("Inserting fail! Arary Queue is Full!!");
+			System.out.println("Inserting fail! Circular Queue is Full!!");
 		else {
-			itemArray[++rear] = item;
+			rear = (rear+1) % this.queueSize;
+			itemArray[rear] = item;
 			System.out.println("Insert Item : " + item);
 		}
 		
@@ -43,10 +44,11 @@ public class ArraryQueue implements Queue {
 	public char deQueue() {
 		// TODO Auto-generated method stub
 		if(isEmpty()) {
-			System.out.println("Deleting fail! Arary Queue is Emtpy!!");
+			System.out.println("Deleting fail! Circular Queue is Emtpy!!");
 			return 0;
 		} else {
-			return itemArray[++front];
+			front = (front + 1) % this.queueSize;
+			return itemArray[front];
 		}
 		
 	}
@@ -55,9 +57,9 @@ public class ArraryQueue implements Queue {
 	public void delete() {
 		// TODO Auto-generated method stub
 		if(isEmpty()) {
-			System.out.println("Deleting fail! Arary Queue is Emtpy!!");			
+			System.out.println("Deleting fail! Circular Queue is Emtpy!!");			
 		} else {
-			++front;
+			front = (front+1) % this.queueSize;
 		}
 	}
 
@@ -65,19 +67,19 @@ public class ArraryQueue implements Queue {
 	public char peek() {
 		// TODO Auto-generated method stub
 		if(isEmpty()) {
-			System.out.println("peeking fail! Arary Queue is Emtpy!!");
+			System.out.println("peeking fail! Circular Queue is Emtpy!!");
 			return 0;
 		} else {
-			return itemArray[front + 1];
+			return itemArray[(front + 1) % this.queueSize];
 		}
 	}
 	
 	public void printQueue(){
 		if(isEmpty()) {
-			System.out.println("Arary Queue is Emtpy!!");			
+			System.out.println("Circular Queue is Emtpy!!");			
 		} else {
-			System.out.printf("Array Queue >> ");
-			for(int i = front +1 ; i <= rear; i++){
+			System.out.printf("Array Circular Queue >> ");
+			for(int i = (front +1) % this.queueSize ; i != (rear+1) % this.queueSize; i=++i % this.queueSize){
 				System.out.printf("%c", itemArray[i]);
 			}
 			System.out.println();
